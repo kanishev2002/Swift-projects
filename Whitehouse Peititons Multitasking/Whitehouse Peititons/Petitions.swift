1c8fb962b944f80120cfd47581c9d2619fc6f6f8
Ilya Kanishev <kanishev2002@gmail.com>
2021-08-16 22:27:54 +0300

    Added Whitehouse Petitions with multitasking

---
+++ Whitehouse Peititons Multitasking/Whitehouse Peititons/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Whitehouse Peititons
+//
+//  Created by Илья Канищев on 11.08.2021.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
